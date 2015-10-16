07c492971b1ba64dc66f0701b9d6881b0bf22b46
Greg Parker <greg@brilliantcollaborations.com>
2015-10-16 11:13:16 -0400

    Initial files

---
+++ www/404.php
@@ -0,0 +1,7 @@
+<?php
+    $page = '404';
+    include 'header.php';
+?>
+
+
+<?php include 'footer.php'; ?>
---
+++ www/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Title</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" type="text/css" href="/assets/styles.css" />
+
+        <!-- Icons -->
+        <link rel="icon" href="/assets/icons/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="/assets/icons/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/icons/icons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/apple-icon-114x14.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-icon-152x152.png">
+    </head>
+    <body>
